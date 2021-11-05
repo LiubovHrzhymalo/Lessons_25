3296a5d93122f1984ae5e8a4f6a22c030fd7914d
LiubovHrzhymalo <lubavasns@gmail.com>
2021-11-05 16:58:10 +0300

    test Jenkins

--- src/test/java/TestLessons_25.java
+++ src/test/java/TestLessons_25.java
@@ -1,37 +1,37 @@
 import Locators.MainPage;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.concurrent.TimeUnit;
 
-public class PageObjectHW {
+public class TestLessons_25 {
     WebDriver driver;
     MainPage mainPage;
-    @BeforeEach
+    @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         mainPage=new MainPage(driver);
     }
-    @AfterEach
+    @After
     public void  tearDown(){
         driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
         driver.close();
     }
-    @Test
+    @org.junit.Test
     public void findByIdTest()  {
         mainPage.openShutterstock();
         mainPage.fieldLogin.sendKeys("test123456");
         mainPage.fieldPassword.sendKeys("test123456");
         System.out.println("Test findByIdTest :The test finds the required ID fields and enters information into them");
     }
-    @Test
+    @org.junit.Test
     public void findByTagNameTest()  {
         mainPage.openAkniga();
         System.out.println("Count frame : " +  mainPage.listFrame.size());
@@ -39,22 +39,22 @@ public class PageObjectHW {
 
     }
 
-    @Test
-    public void findByLinkTest()  {
-        mainPage.openShutterstockZaretsky();
-        mainPage.wait.until(ExpectedConditions.elementToBeClickable(mainPage.buttonNext));
-        mainPage.buttonNext.click();
-        System.out.println("Test findByLinkText: The test finds the necessary" +
-                " link for the \"Далее\" button, clicks on the button and goes to the next page");
-    }
-    @Test
+    //    @Test
+//    public void findByLinkTest()  {
+//        mainPage.openShutterstockZaretsky();
+//        mainPage.wait.until(ExpectedConditions.elementToBeClickable(mainPage.buttonNext));
+//        mainPage.buttonNext.click();
+//        System.out.println("Test findByLinkText: The test finds the necessary" +
+//                " link for the \"Далее\" button, clicks on the button and goes to the next page");
+//    }
+    @org.junit.Test
     public void findByCssSelectorTest()  {
         mainPage.openAkniga();
         mainPage.wait.until(ExpectedConditions.visibilityOf(mainPage.svgPicture));
         System.out.println("Test findByCssSelectorTest: The test finds a code element" +
                 " according to the given css selectors path");
     }
-    @Test
+    @org.junit.Test
     public void findCssSelectorChildElementTest()  {
         mainPage.openAkniga();
         mainPage.wait.until(ExpectedConditions.elementToBeClickable(mainPage.buttonLink));
@@ -62,24 +62,24 @@ public class PageObjectHW {
         System.out.println("Test findByCssSelectorTest: The test finds a code element" +
                 " according to the given css selectors path");
     }
-    @Test
+    @org.junit.Test
     public void findByXpathTest() {
         mainPage.openAkniga();
         System.out.println("Count Span : " + mainPage.listSpan.size());
         System.out.println("Test findByXpathTest: " +
                 "The test finds the required elements tag and counts the number of items");
     }
-    @Test
+    @org.junit.Test
     public void findByXpathAttributeTest()  {
         mainPage.openShutterstockZaretskyIMG();
         mainPage.wait.until(ExpectedConditions.visibilityOf(mainPage.searchByAttribute));
         System.out.println("Test findByXpathAttributeTest: The test finds the required alt attribute Image");
     }
-    @Test
-    public void findByXpathTextTest() {
-        mainPage.openShutterstockZaretskyIMG();
-        mainPage.wait.until(ExpectedConditions.textToBePresentInElement(mainPage.searchByText,"Информация"));
-        mainPage.searchByText.click();
-        System.out.println("Test findByXpathTextTest: The test found the 'Information' button and opened the page");
-    }
+//    @Test
+//    public void findByXpathTextTest() {
+//        mainPage.openShutterstockZaretskyIMG();
+//        mainPage.wait.until(ExpectedConditions.textToBePresentInElement(mainPage.searchByText,"Информация"));
+//        mainPage.searchByText.click();
+//        System.out.println("Test findByXpathTextTest: The test found the 'Information' button and opened the page");
+//    }
 }
